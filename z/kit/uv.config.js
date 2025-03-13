4bdffee1416cd579dedb6cfdf4e6e83a0c3b8953
above <160554522+Abovescore@users.noreply.github.com>
2025-03-12 23:41:50 -0500

    the devious z folder arrives

---
+++ z/kit/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/z/-kit/',
+    bare:'https://goto.zkit.above.gay/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/z/kit/uv.handler.js',
+    bundle: '/z/kit/uv.bundle.js',
+    config: '/z/kit/uv.config.js',
+    sw: '/z/kit/uv.sw.js',
+};
