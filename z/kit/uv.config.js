5c3e81dbfd0a9cadd4aa673ab678e51f8c82de9b
wydsculted <aidanjoshua934@gmail.com>
2025-03-23 18:36:16 -0400

    Update uv.config.js

--- z/kit/uv.config.js
+++ z/kit/uv.config.js
@@ -1,10 +1,11 @@
 self.__uv$config = {
-    prefix: '/z/-kit/',
-    bare:'https://goto.zkit.above.gay/bare/',
+    prefix: '/securly/who/',
+    bare:'https://goto.securlywho.unblocked.gg/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/z/kit/uv.handler.js',
-    bundle: '/z/kit/uv.bundle.js',
-    config: '/z/kit/uv.config.js',
-    sw: '/z/kit/uv.sw.js',
+    handler: '/securly/who/uv.handler.js',
+    bundle: '/securly/who/uv.bundle.js',
+    bundle: '/securly/who/uv.bundle.js',
+    config: '/securly/who/uv.config.js',
+    sw: '/securly/who/uv.sw.js',
 };
